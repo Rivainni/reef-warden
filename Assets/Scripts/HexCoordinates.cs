48b01aba1870df0ddf21d38da52790c286e30dae
Anton Ralph F. Valenzuela <antonralphvalenzuela68@gmail.com>
2021-09-04 07:03:16 +0800

    Generated hex grid.

---
+++ Assets/Scripts/HexCoordinates.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+[System.Serializable]
+public struct HexCoordinates
+{
+    [SerializeField] int x, z;
+    public int X
+    {
+        get
+        {
+            return x;
+        }
+    }
+
+    public int Y
+    {
+        get
+        {
+            return -X - Z;
+        }
+    }
+    public int Z
+    {
+        get
+        {
+            return z;
+        }
+    }
+
+    public HexCoordinates(int x, int z)
+    {
+        this.x = x;
+        this.z = z;
+    }
+
+    public static HexCoordinates FromOffsetCoordinates(int x, int z)
+    {
+        return new HexCoordinates(x, z - x / 2);
+    }
+
+    public override string ToString()
+    {
+        return "(" + X.ToString() + ", " + Z.ToString() + ")";
+    }
+
+    public string ToStringOnSeparateLines()
+    {
+        return X.ToString() + "\n" + Z.ToString();
+    }
+
+    public static HexCoordinates FromPosition(Vector3 position)
+    {
+        float x = position.x / (HexMetrics.outerRadius * 1.5f);
+        float y = -x;
+
+        float offset = position.z / (HexMetrics.innerRadius * 4f);
+        x -= offset;
+        y -= offset;
+
+        int iX = Mathf.RoundToInt(x);
+        int iY = Mathf.RoundToInt(y);
+        int iZ = Mathf.RoundToInt(-x - y);
+
+        if (iX + iY + iZ != 0)
+        {
+            Debug.LogWarning("Rounding error!");
+        }
+
+        return new HexCoordinates(iX, iZ);
+    }
+}
