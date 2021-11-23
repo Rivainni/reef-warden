a02f800957afd5625bf0d7cbe7feb9edf886470a
Anton Ralph F. Valenzuela <antonralphvalenzuela68@gmail.com>
2021-11-24 03:14:00 +0800

    Trying to get clamping to work

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -23,10 +23,19 @@ public class CameraController : MonoBehaviour
     Vector3 rotateStartPosition;
     Vector3 rotateCurrentPosition;
 
+    float minX;
+    float minY;
+    float minZ;
+    float maxX;
+    float maxY;
+    float maxZ;
+
     void Start()
     {
+        SetClamps();
         newPosition = transform.position;
         newRotation = transform.rotation;
+        // newZoom = new Vector3(cameraTransform.localPosition.x, cameraTransform.localPosition.y + 20.0f, cameraTransform.localPosition.z);
         newZoom = cameraTransform.localPosition;
     }
 
@@ -72,24 +81,6 @@ public class CameraController : MonoBehaviour
         //     }
         // }
 
-        // edge movement
-        if (Input.mousePosition.y > Screen.height - edgeSize)
-        {
-            newPosition += transform.forward * movementSpeed;
-        }
-        if (Input.mousePosition.y < edgeSize)
-        {
-            newPosition += transform.forward * -movementSpeed;
-        }
-        if (Input.mousePosition.x < edgeSize)
-        {
-            newPosition += transform.right * -movementSpeed;
-        }
-        if (Input.mousePosition.x > Screen.width - edgeSize)
-        {
-            newPosition += transform.right * movementSpeed;
-        }
-
         if (Input.GetMouseButtonDown(2))
         {
             rotateStartPosition = Input.mousePosition;
@@ -107,6 +98,7 @@ public class CameraController : MonoBehaviour
 
     void HandleMovementInput()
     {
+        // fast movement
         if (Input.GetKey(KeyCode.LeftShift))
         {
             movementSpeed = fastSpeed;
@@ -116,6 +108,7 @@ public class CameraController : MonoBehaviour
             movementSpeed = normalSpeed;
         }
 
+        // keyboard movement
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             newPosition += transform.forward * movementSpeed;
@@ -129,6 +122,24 @@ public class CameraController : MonoBehaviour
             newPosition += transform.right * -movementSpeed;
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            newPosition += transform.right * movementSpeed;
+        }
+
+        // edge movement
+        if (Input.mousePosition.y > Screen.height - edgeSize)
+        {
+            newPosition += transform.forward * movementSpeed;
+        }
+        if (Input.mousePosition.y < edgeSize)
+        {
+            newPosition += transform.forward * -movementSpeed;
+        }
+        if (Input.mousePosition.x < edgeSize)
+        {
+            newPosition += transform.right * -movementSpeed;
+        }
+        if (Input.mousePosition.x > Screen.width - edgeSize)
         {
             newPosition += transform.right * movementSpeed;
         }
@@ -152,9 +163,23 @@ public class CameraController : MonoBehaviour
             newZoom -= zoomAmount;
         }
 
+        newPosition = new Vector3(Mathf.Clamp(newPosition.x, minX, maxX), newPosition.y, Mathf.Clamp(newPosition.z, minZ, maxZ));
+        newZoom = new Vector3(newZoom.x, Mathf.Clamp(newZoom.y, minY, maxY), newZoom.z);
+
         // smooth movement, have to linear interpolate
-        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
-        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * movementTime);
-        cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, Time.deltaTime * movementTime);
+        transform.position = Vector3.Lerp(transform.position, newPosition, 0.5f);
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, 0.5f);
+        cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, 0.5f);
+    }
+
+    void SetClamps()
+    {
+        // hardcode for now
+        minX = 0.0f;
+        minY = 0.0f;
+        minZ = 0.0f;
+        maxX = 415f;
+        maxY = 70.0f;
+        maxZ = 360f;
     }
 }
