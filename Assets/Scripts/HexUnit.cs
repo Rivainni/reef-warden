a9677843504c1a043ceb717518723aeeb6aa6929
Anton Ralph F. Valenzuela <antonralphvalenzuela68@gmail.com>
2022-04-09 14:44:41 +0800

    Despawn bug fix

--- Assets/Scripts/HexUnit.cs
+++ Assets/Scripts/HexUnit.cs
@@ -140,7 +140,10 @@ public class HexUnit : MonoBehaviour
     {
         location.Unit = null;
         Grid.GetAudioManager().Stop("Boat");
-        waypoint.Die();
+        if (waypoint)
+        {
+            waypoint.Die();
+        }
         Destroy(gameObject);
     }
 
